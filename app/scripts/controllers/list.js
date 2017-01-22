e26da12b8a6e7a0e2a57e0207b1de372de16f256
Ubuntu <ubuntu@ip-10-0-0-191.us-west-2.compute.internal>
2017-01-22 18:22:02 +0000

    complete with all backend depended features.  checking in to clean up in an IDE and sort-by features

--- app/scripts/controllers/list.js
+++ app/scripts/controllers/list.js
@@ -8,10 +8,25 @@
  * Controller of the angularS3App
  */
 angular.module('angularS3App')
-  .controller('ListCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('ListCtrl', function ($scope, $http) {
+    $scope.files = [];
+    $http.get('http://54.191.47.109:9194/api/v1/list').
+        then(function(response) {
+console.log(response.data);
+            for( var file in response.data['files']){
+console.log(response.data['files'][file]);
+               $scope.files.push({"file": response.data['files'][file][0], "date": response.data['files'][file][1]});}
+            $scope.bucket = response.data['bucket'];
+            $scope.folder = response.data['folder'];
+        });
+$scope.delete = function ( idx ) {
+  var file_to_delete = $scope.files[idx];
+  var delete_url = 'http://54.191.47.109:9194/api/v1/delete?file='+file_to_delete;
+console.log('deleting file '+ file_to_delete);
+console.log('with url' + delete_url);
+            // have to use GET instead of DELETE because angular is weird
+            $http.delete(delete_url);
+    $scope.files.splice(idx, 1);
+  };
+});
+
