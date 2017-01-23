36b448d05f4813c3a677934aefc476f110add7f9
root <root@ip-10-0-0-191.us-west-2.compute.internal>
2017-01-23 06:31:09 +0000

    added columns for size and extension that sort. about page has total_use

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -10,15 +10,19 @@
 angular.module('angularS3App')
     .controller('AboutCtrl', function ($scope, $http) {
         $scope.files = [];
+        //$http.get('http://s3.sfblur.com/api/v1/list').then(function (response) {
         $http.get('http://s3.sfblur.com/api/v1/list').then(function (response) {
             console.log(response.data);
+            var total_use = 0;
             for (var file in response.data['files']) {
                 console.log(response.data['files'][file]);
                 $scope.files.push({"file": response.data['files'][file][0], "date": response.data['files'][file][1]});
+                total_use = total_use + response.data['files'][file][2]
             }
             $scope.bucket = response.data['bucket'];
             $scope.folder = response.data['folder'];
             $scope.backend_url = 'http://s3.sfblur.com/';
+            $scope.total_use = total_use;
         });
     });
 
--- app/scripts/controllers/list.js
+++ app/scripts/controllers/list.js
@@ -10,11 +10,12 @@
 angular.module('angularS3App')
     .controller('ListCtrl', function ($scope, $http) {
         $scope.files = [];
+        // $http.get('http://54.191.47.109:9194/api/v1/list').then(function (response) {
         $http.get('http://s3.sfblur.com/api/v1/list').then(function (response) {
             console.log(response.data);
             for (var file in response.data['files']) {
                 console.log(response.data['files'][file]);
-                $scope.files.push({"file": response.data['files'][file][0], "date": response.data['files'][file][1]});
+                $scope.files.push({"file": response.data['files'][file][0], "date": response.data['files'][file][1], "size": response.data['files'][file][2], "extension": response.data['files'][file][3]});
             }
             $scope.bucket = response.data['bucket'];
             $scope.folder = response.data['folder'];
@@ -25,6 +26,7 @@ angular.module('angularS3App')
              */
             var file_to_delete = $scope.files[idx]['file'];
             var delete_url = 'http://s3.sfblur.com/api/v1/delete?file=' + file_to_delete;
+            // var delete_url = 'http://54.191.47.109:9194/api/v1/delete?file=' + file_to_delete;
             console.log('Deleting file ' + file_to_delete);
             console.log('  with url' + delete_url);
             // have to use GET instead of DELETE because angular is weird
